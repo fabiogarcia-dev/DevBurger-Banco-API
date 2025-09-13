ebb5fa014aa20b23ddd9d524cecd3485f10504a2
fabiogarcia-dev <fabio.ti.am@gmail.com>
2025-09-12 23:16:15 -0400

    criação controller para pagamento e serviço do stripe

--- src/services/stripe.js
+++ src/services/stripe.js
@@ -1,6 +1,8 @@
 import Stripe from 'stripe';
+import dotenv from 'dotenv';
 
-// coloque sua chave secreta diretamente aqui
-const stripe = new Stripe('sk_test_51S5xCq0wYwlYCLk6C0gCACsvz24rzcYQ7V8vb6SWKY9H249jlaQC0JwAzAklQhM4ABFI7onzN7DPDyp4h7YStUf500Qz1iKBZw');
+dotenv.config(); // Carrega variáveis do .env
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 export default stripe;
